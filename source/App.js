90a8379be9dd3091d6536ed2da17bee8275cad7b
Stanley Krute <stan@stankrute.com>
2013-02-13 07:52:59 -0800

    progress bar via

--- source/App.js
+++ source/App.js
@@ -1,17 +1,28 @@
 enyo.kind({
-	name: "App",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    name: "OnyxSample",
+    components: [
+        { kind: "onyx.Toolbar", components: [
+            { content: "Toolbar" },
+            { kind: "onyx.Button", content: "Toolbar Button" }
+        ]},
+        { content: "Radio Group" },
+        { kind: "onyx.RadioGroup", onActivate: "activated", components: [
+            { content: "One", active: true },
+            { content: "Two" },
+            { content: "Three" }
+        ]},
+        { content: "Groupbox" },
+        { kind: "onyx.Groupbox", components: [
+            { kind: "onyx.GroupboxHeader", content: "Groupbox Header" },
+            { content: "Groupbox item" }
+        ]},
+        { content: "ProgressBar" },
+        { kind: "onyx.ProgressBar", progress: -1 }
+    ],
+    activated: function(inSender, inEvent) {
+        // React to radio button activation change
+		foo = this.$.progressBar.progress ;
+		this.$.progressBar.animateProgressTo(foo + 1) ;
+    }
 });
+
